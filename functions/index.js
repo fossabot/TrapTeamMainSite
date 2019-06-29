21d6a52d149abe48fe9bd52d6325abbe906776de
Ezrashaw <Ezrashaw1414@gmail.com>
2019-06-29 14:33:26 +1200

    feat FBFunctions: Added ToggleTrap Function
    First Version of ToggleTrap

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,59 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp()
+var functions = require('firebase-functions');
+var admin = require('firebase-admin');
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://trapteam-cc.firebaseio.com"
+});
 
 exports.FillTrap = functions.https.onRequest((req, res) => {
-  if (req.method === "POST") {
-    var TrapHolder = req.body.owner;
-    var TrapName = req.body.name;
-    admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Full");
-    console.log("Filled " + TrapHolder + "'s Trap.");
-    res.status(200).send("Filled " + TrapHolder + "'s Trap.");
-  } else {
+  if (req.method != "POST") {
     console.error('Not POST: ' + req.method);
     res.status(405).send("Error, Must send with POST not: " + req.method);
   }
+  var TrapHolder = req.body.owner;
+  var TrapName = req.body.name;
+  admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Full");
+  console.log("Filled " + TrapHolder + "'s Trap.");
+  res.status(200).send("Filled " + TrapHolder + "'s Trap.");
 });
 
 exports.EmptyTrap = functions.https.onRequest((req, res) => {
-  if (req.method === "POST") {
-    var TrapHolder = req.body.owner;
-    var TrapName = req.body.name;
-    admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Empty");
-    console.log("Emptyed " + TrapHolder + "'s Trap.");
-    res.status(200).send("Emptyed " + TrapHolder + "'s Trap.");
-  } else {
+  if (req.method != "POST") {
     console.error('Not POST: ' + req.method);
     res.status(405).send("Error, Must send with POST not: " + req.method);
   }
+  var TrapHolder = req.body.owner;
+  var TrapName = req.body.name;
+  admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Empty");
+  console.log("Emptyed " + TrapHolder + "'s Trap.");
+  res.status(200).send("Emptyed " + TrapHolder + "'s Trap.");
+});
+
+exports.ToggleTrap = functions.https.onRequest((req, res) => {
+  if (req.method != "POST") {
+    console.error('Not POST: ' + req.method);
+    res.status(405).send("Error, Must send with POST not: " + req.method);
+  }
+  var TrapHolder = req.body.owner;
+  var TrapName = req.body.name;
+  admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").once('value', (snapshot) => {
+    var Value = snapshot.val();
+    if (Value == "Full") {
+      admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Empty");
+      console.log("Emptyed " + TrapHolder + "'s Trap.");
+      res.status(200).send("Emptyed " + TrapHolder + "'s Trap.");
+    }
+    else if (Value == "Empty") {
+      admin.database().ref("/Trapholders/" + TrapHolder + "/Traps/" + TrapName + "/Status").set("Full");
+      console.log("Filled " + TrapHolder + "'s Trap.");
+      res.status(200).send("Filled " + TrapHolder + "'s Trap.");
+    }
+    else {
+      res.status(400).send("You mucked up!!!");
+    }
+  });
 });
 
 exports.addAccount = functions.auth.user().onCreate((user) => {
